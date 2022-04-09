095e7175b170cfc653fdd44d11abe3ea65b68f6d
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-04-10 01:02:14 +0300

    added colors.h file

---
+++ Student Run/colors.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <GL/freeglut.h>
+
+struct Color {
+	GLfloat red;
+	GLfloat green;
+	GLfloat blue;
+
+	Color(GLfloat red, GLfloat green, GLfloat blue) {
+		this->red = red;
+		this->green = green;
+		this->blue = blue;
+	}
+};
+
+// basic colors
+Color white(1.0f, 1.0f, 1.0f);
+Color black(0.0f, 0.0f, 0.0f);
+
+// metro background colors
+Color metroFloorColor(0.22f, 0.28f, 0.44f);
+Color metroFloorShadeColor(0.31f, 0.37f, 0.51f);
+Color metroMetricsBackgroundColor(0.93f, 0.95f, 0.92f);
+Color metroVentilationColor(0.59f, 0.84f, 0.94f);
+Color metroDoorColor(0.32f, 0.55f, 0.61f);
+Color bottomMetroDoorColor(0.72f, 0.73f, 0.77f);
+Color metroWindowColor(0.14f, 0.15f, 0.16f);
+Color metroBackrestColor(0.42f, 0.53f, 0.65f);
+Color metroBackrestShadeColor(0.58f, 0.66f, 0.76f);
+Color metroSeatColor(0.42f, 0.53f, 0.65f);
+Color metroSeatShadeColor(0.58f, 0.66f, 0.76f);
+Color metroHandleColor(0.56f, 0.26f, 0.23f);
+Color metroBarColor(0.69f, 0.81f, 0.82f);
+Color metroCarMargin(0.40f, 0.40f, 0.41f);
