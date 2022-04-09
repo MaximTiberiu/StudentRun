9eacfa820a64188b6744850d82b20468c54eb688
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-04-09 21:59:04 +0300

    added utils.h - #11

---
+++ Student Run/utils.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <GL/freeglut.h>
+#include <math.h>
+
+#define PI 3.14
+
+struct Color {
+	GLfloat red;
+	GLfloat green;
+	GLfloat blue;
+
+	Color(GLfloat red, GLfloat green, GLfloat blue) {
+		this->red = red;
+		this->green = green;
+		this->blue = blue;
+	}
+};
+
+void renderString(float x, float y, void* font, const unsigned char* string, Color color)
+{
+	glColor3f(color.red, color.green, color.blue);
+	glRasterPos2f(x, y);
+	glutBitmapString(font, string);
+}
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat radius, Color color) {
+	int numberOfTriangles = 20;
+	GLfloat doublePI = 2.0f * PI;
+
+	glColor3f(color.red, color.green, color.blue);
+
+	glBegin(GL_TRIANGLE_FAN);
+	
+	glVertex2f(x, y); // center of circle
+	for (int i = 0; i <= numberOfTriangles; i++) {
+		glVertex2f(x + (radius * cos(i * doublePI / numberOfTriangles)), y + (radius * sin(i * doublePI / numberOfTriangles)));
+	}
+
+	glEnd();
+}
+
+void drawHollowCircle(GLfloat x, GLfloat y, GLfloat radius, Color color) {
+	int numberOfLines = 100;
+	GLfloat doublePI = 2.0f * PI;
+
+	glColor3f(color.red, color.green, color.blue);
+
+	glBegin(GL_LINE_LOOP);
+
+	for (int i = 0; i <= numberOfLines; i++) {
+		glVertex2f(x + (radius * cos(i * doublePI / numberOfLines)), y + (radius * sin(i * doublePI / numberOfLines)));
+	}
+
+	glEnd();
+}
+
+void drawRectangle(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, Color color) {
+	glBegin(GL_QUADS);
+
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y1);
+	glVertex2f(x2, y2);
+	glVertex2f(x1, y2);
+
+	glEnd();
+}
+
+void drawHollowRectangle(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, Color color) {
+	glBegin(GL_LINE_LOOP);
+
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y1);
+	glVertex2f(x2, y2);
+	glVertex2f(x1, y2);
+
+	glEnd();
+}
+
+void drawTriangle(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, GLfloat x3, GLfloat y3, Color color) {
+	glBegin(GL_TRIANGLES);
+
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y2);
+	glVertex2f(x3, y3);
+
+	glEnd();
+}
+
+void drawHollowTriangle(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, GLfloat x3, GLfloat y3, Color color) {
+	glBegin(GL_LINE_LOOP);
+
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y2);
+	glVertex2f(x3, y3);
+
+	glEnd();
+}
